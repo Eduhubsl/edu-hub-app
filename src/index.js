4085298f87ec5902ead733eb90a62cd4eed71b39
Wikum <wik.dburn@gmail.com>
2025-07-07 20:43:40 +0530

    Fix syntax error in index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
-import App from './App';
-import './index.css'; // You can remove this line if you're not using CSS
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-=======
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -22,4 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> c961dcaf374378fcf2f1fb15a828fff33f68e0b1
